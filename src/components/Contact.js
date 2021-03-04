17c4bf34cf7f6a981c9f3a06d726ca5f9c4be736
RobLazCDMX <66651813+RobLazCDMX@users.noreply.github.com>
2021-03-04 09:09:40 -0600

    Update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
             
             </div>
             <div className="cv-btn-contact" >
-                <a href='#'>Continue</a> 
+                <a href='https://roblaz-portfolio.netlify.app/'>Continue</a> 
             </div>
             <div className="cv-btn-contact" >
                 <a href="" onClick={()=> window.open("https://slack-files.com/T5VCY49B3-F01MY9M27U3-3bbdfe3f2a", "_blank")}>Download CV</a>
